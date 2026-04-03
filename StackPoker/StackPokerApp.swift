40119b197796ffe718be580237775c8dd04395b8
camronjacobson <72469929+camronjacobson@users.noreply.github.com>
2026-04-02 19:11:00 -0700

    Initial Commit

---
+++ StackPoker/StackPokerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackPokerApp.swift
+//  StackPoker
+//
+//  Created by Camron Jacobson on 4/2/26.
+//
+
+import SwiftUI
+
+@main
+struct StackPokerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
